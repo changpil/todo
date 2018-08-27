33e7ec305c9037555b7dbe75733b52e96ecea2bf
Changpil Lee <changpil@gmail.com>
2018-08-27 18:31:22 +0000

    running status

---
+++ src/todo/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from todo.models import TodoItem
+
+
+class TodoItemSerializer(serializers.ModelSerializer):
+    #pk = serializers.Field()
+    #title = serializers.CharField(max_length=256, required=False, blank=True)
+    #completed = serializers.BooleanField(required=False, default=False)
+    #url = serializers.CharField(max_length=256, required=False, blank=True)
+    #order = serializers.IntegerField(required=False, blank=True)
+
+    #def restore_object(self, attrs, instance=None):
+    #    if instance:
+    #        instance.title = attrs.get('title', instance.title)
+    #        instance.completed = attrs.get('completed', instance.completed)
+    #        instance.url = attrs.get('url', instance.url)
+    #        instance.order = attrs.get('order', instance.order)
+    #        return instance
+    #    return TodoItem(**attrs)
+    
+    url = serializers.ReadOnlyField()
+    class Meta:
+        model = TodoItem
+        fields = ('title', 'completed', 'url', 'order')
